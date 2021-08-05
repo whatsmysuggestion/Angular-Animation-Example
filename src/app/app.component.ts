eeb7af3e07e3364fa803e3a6610ac556461a904a
whatsmysuggestion <whatsmysuggestion@gmail.com>
2021-08-05 14:08:09 +0530

    Angular Animation Code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+styles:[`
+      div{
+         margin: 0 auto;
+         text-align: center;
+         width:200px;
+      }
+      .rotate{
+         width:100px;
+         height:100px;
+		 margin-top:200px;
+         border:solid 1px red;
+      }
+   `],
+   animations: [
+      trigger('myanimation',[
+         state('smaller',style({
+            transform : 'translate(1px,1px)'
+         })),
+         state('larger',style({
+            transform :'translate(300px,300px)'
+         })),
+         transition('smaller <=> larger',animate('3000ms ease-out'))
+      ])
+   ]
 })
+
 export class AppComponent {
-  title = 'sample9';
+    state: string = "smaller";
+   animate() {
+      this.state= this.state == 'larger' ? 'smaller' : 'larger';
+   }
 }
